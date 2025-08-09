5978a4c373fb83067924b89f672a5bf28229eaf8
Toriqul Muchlissin <tmuchlissin@gmail.com>
2025-08-09 10:59:51 +0700

    update: adjustment view camera

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,19 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, ValidationError
 import re
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SelectField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+import re
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SelectField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+import re
+
 class CameraForm(FlaskForm):
     location = StringField('Location', validators=[DataRequired()])
-    ip_address = StringField('IP Address', validators=[DataRequired()])
+    ip_address = StringField('IP Address/Stream URL', validators=[DataRequired()])
     status = BooleanField('Status (On/Off)', default=False)
     type = SelectField(
         'CCTV Type',
@@ -15,16 +25,42 @@ class CameraForm(FlaskForm):
     )
 
     def validate_ip_address(form, field):
-        ip_pattern = r"^http:\/\/(?:\d{1,3}\.){3}\d{1,3}(?::\d{1,5})?(?:\/video)?$"
-        if not re.match(ip_pattern, field.data):
+        http_pattern = r"^http:\/\/(?:(?:\d{1,3}\.){3}\d{1,3}|[\w\-\.]+)(?::\d{1,5})?(?:\/video)?$"
+        
+        rtsp_pattern = r"^rtsp:\/\/(?:[\w]+:[\w]+@)?(?:(?:\d{1,3}\.){3}\d{1,3}|[\w\-\.]+)(?::\d{1,5})?(?:\/[\w\/\-\.]+)?(?:\?[\w=&\-]+)?$"
+        
+        if re.match(http_pattern, field.data):
+            url_without_protocol = field.data.split('//')[1]
+            host_portion = url_without_protocol.split('/')[0]
+            host_only = host_portion.split(':')[0]
+            
+            if re.match(r"^(?:\d{1,3}\.){3}\d{1,3}$", host_only):
+                segments = host_only.split('.')
+                if any(not segment.isdigit() or not (0 <= int(segment) <= 255) for segment in segments):
+                    raise ValidationError('Invalid IP Address format. IP segments should be between 0 and 255.')
+    
+        elif re.match(rtsp_pattern, field.data):
+            url_without_protocol = field.data.split('//')[1]
+
+            if '@' in url_without_protocol:
+                host_portion = url_without_protocol.split('@')[1]
+            else:
+                host_portion = url_without_protocol
+            
+            host_only = host_portion.split(':')[0].split('/')[0]
+
+            if re.match(r"^(?:\d{1,3}\.){3}\d{1,3}$", host_only):
+                segments = host_only.split('.')
+                if any(not segment.isdigit() or not (0 <= int(segment) <= 255) for segment in segments):
+                    raise ValidationError('Invalid IP Address format. IP segments should be between 0 and 255.')
+        
+        else:
             raise ValidationError(
-                'IP Address must follow the format: "http://xxx.xxx.xxx.xxx[:port]/video". Example: http://192.168.1.106:4747/video.'
+                'Stream URL must follow HTTP or RTSP format:\n'
+                'HTTP: "http://host[:port]/video" (e.g., http://192.168.1.106:4747/video, http://localhost:8080/video)\n'
+                'RTSP: "rtsp://[username:password@]host[:port][/path][?params]" '
+                '(e.g., rtsp://localhost:8554/mystream/test, rtsp://192.168.1.111/MediaInput/h264)'
             )
-        ip_portion = field.data.split('//')[1].split('/')[0]
-        ip_only = ip_portion.split(':')[0]
-        segments = ip_only.split('.')
-        if len(segments) != 4 or any(not segment.isdigit() or not (0 <= int(segment) <= 255) for segment in segments):
-            raise ValidationError('Invalid IP Address format. IP segments should be between 0 and 255.')
 
     submit = SubmitField('Add CCTV')
 
